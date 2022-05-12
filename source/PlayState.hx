a3c6ac8e2531bc69461b1eafda06211653a27dd4
Gab's Swag Alt <104793558+Gabriel2019rsAlt@users.noreply.github.com>
2022-05-12 16:35:33 +0200

    idk disruption mod chart thing

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2376,6 +2376,77 @@ class PlayState extends MusicBeatState
 
 		super.update(elapsed);
 
+	    	if (SONG.song.toLowerCase() == 'disruption') // deez all day
+				{
+				var krunkThing = 60;
+	
+				poop.alpha = Math.sin(elapsedtime) / 2.5 + 0.4;
+	
+				playerStrums.forEach(function(spr:FlxSprite)
+				{
+					spr.x = arrowJunks[spr.ID + 4][0] + (Math.sin(elapsedtime) * ((spr.ID % 2) == 0 ? 1 : -1)) * krunkThing;
+					spr.y = arrowJunks[spr.ID + 4][1] + Math.sin(elapsedtime - 5) * ((spr.ID % 2) == 0 ? 1 : -1) * krunkThing;
+	
+					spr.scale.x = Math.abs(Math.sin(elapsedtime - 5) * ((spr.ID % 2) == 0 ? 1 : -1)) / 4;
+	
+					spr.scale.y = Math.abs((Math.sin(elapsedtime) * ((spr.ID % 2) == 0 ? 1 : -1)) / 2);
+
+					spr.scale.x += 0.2;
+					spr.scale.y += 0.2;
+
+					spr.scale.x *= 1.5;
+					spr.scale.y *= 1.5;
+				});
+				opponentStrums.forEach(function(spr:FlxSprite)
+				{
+					spr.x = arrowJunks[spr.ID][0] + (Math.sin(elapsedtime) * ((spr.ID % 2) == 0 ? 1 : -1)) * krunkThing;
+					spr.y = arrowJunks[spr.ID][1] + Math.sin(elapsedtime - 5) * ((spr.ID % 2) == 0 ? 1 : -1) * krunkThing;
+	
+					spr.scale.x = Math.abs(Math.sin(elapsedtime - 5) * ((spr.ID % 2) == 0 ? 1 : -1)) / 4;
+	
+					spr.scale.y = Math.abs((Math.sin(elapsedtime) * ((spr.ID % 2) == 0 ? 1 : -1)) / 2);
+	
+					spr.scale.x += 0.2;
+					spr.scale.y += 0.2;
+	
+					spr.scale.x *= 1.5;
+		     		spr.scale.y *= 1.5;
+				});
+	
+				notes.forEachAlive(function(spr:Note){
+					if (spr.mustPress) {
+						spr.x = arrowJunks[spr.noteData + 4][0] + (Math.sin(elapsedtime) * ((spr.noteData % 2) == 0 ? 1 : -1)) * krunkThing;
+						spr.y = arrowJunks[spr.noteData + 4][1] + Math.sin(elapsedtime - 5) * ((spr.noteData % 2) == 0 ? 1 : -1) * krunkThing;
+
+						spr.scale.x = Math.abs(Math.sin(elapsedtime - 5) * ((spr.noteData % 2) == 0 ? 1 : -1)) / 4;
+
+						spr.scale.y = Math.abs((Math.sin(elapsedtime) * ((spr.noteData % 2) == 0 ? 1 : -1)) / 2);
+	
+						spr.scale.x += 0.2;
+						spr.scale.y += 0.2;
+
+						spr.scale.x *= 1.5;
+						spr.scale.y *= 1.5;
+						}
+				     	else
+					    {
+						spr.x = arrowJunks[spr.noteData][0] + (Math.sin(elapsedtime) * ((spr.noteData % 2) == 0 ? 1 : -1)) * krunkThing;
+						spr.y = arrowJunks[spr.noteData][1] + Math.sin(elapsedtime - 5) * ((spr.noteData % 2) == 0 ? 1 : -1) * krunkThing;
+
+						spr.scale.x = Math.abs(Math.sin(elapsedtime - 5) * ((spr.noteData % 2) == 0 ? 1 : -1)) / 4;
+	
+						spr.scale.y = Math.abs((Math.sin(elapsedtime) * ((spr.noteData % 2) == 0 ? 1 : -1)) / 2);
+	
+						spr.scale.x += 0.2;
+						spr.scale.y += 0.2;
+	
+						spr.scale.x *= 1.5;
+						spr.scale.y *= 1.5;
+					}
+				});
+	    	}
+		
+
 		if(ratingName == '?') {
 			scoreTxt.text = 'Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingName;
 		} else {
