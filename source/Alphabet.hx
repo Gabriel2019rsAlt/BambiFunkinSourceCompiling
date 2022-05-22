85c8d71bd7afcdaddf6fe366ef8a48b1458f1fd7
Gab's Swag Alt <104793558+Gabriel2019rsAlt@users.noreply.github.com>
2022-05-22 19:33:16 +0200

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -7,6 +7,7 @@ import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxMath;
 import flixel.util.FlxTimer;
 import flixel.system.FlxSound;
+import flash.media.Sound;
 
 using StringTools;
 
@@ -41,7 +42,7 @@ class Alphabet extends FlxSpriteGroup
 
 	var splitWords:Array<String> = [];
 
-	var isBold:Bool = false;
+	public var isBold:Bool = false;
 	public var lettersArray:Array<AlphaCharacter> = [];
 
 	public var finishedText:Bool = false;
@@ -78,6 +79,7 @@ class Alphabet extends FlxSpriteGroup
 	{
 		for (i in 0...lettersArray.length) {
 			var letter = lettersArray[0];
+			letter.destroy();
 			remove(letter);
 			lettersArray.remove(letter);
 		}
@@ -123,7 +125,7 @@ class Alphabet extends FlxSpriteGroup
 			// {
 			// }
 
-			var spaceChar:Bool = (character == " " || character == "_");
+			var spaceChar:Bool = (character == " " || (isBold && character == "_"));
 			if (spaceChar)
 			{
 				consecutiveSpaces++;
@@ -198,7 +200,14 @@ class Alphabet extends FlxSpriteGroup
 	var xPos:Float = 0;
 	public var curRow:Int = 0;
 	var dialogueSound:FlxSound = null;
+	private static var soundDialog:Sound = null;
 	var consecutiveSpaces:Int = 0;
+	public static function setDialogueSound(name:String = '')
+	{
+		if (name == null || name.trim() == '') name = 'dialogue';
+		soundDialog = Paths.sound(name);
+		if(soundDialog == null) soundDialog = Paths.sound('dialogue');
+	}
 
 	var typeTimer:FlxTimer = null;
 	public function startTypedText(speed:Float):Void
@@ -208,12 +217,17 @@ class Alphabet extends FlxSpriteGroup
 
 		// trace(arrayShit);
 
+		if(soundDialog == null)
+		{
+			Alphabet.setDialogueSound();
+		}
+
 		if(speed <= 0) {
 			while(!finishedText) { 
 				timerCheck();
 			}
 			if(dialogueSound != null) dialogueSound.stop();
-			dialogueSound = FlxG.sound.play(Paths.sound('dialogue'));
+			dialogueSound = FlxG.sound.play(soundDialog);
 		} else {
 			typeTimer = new FlxTimer().start(0.1, function(tmr:FlxTimer) {
 				typeTimer = new FlxTimer().start(speed, function(tmr:FlxTimer) {
@@ -244,7 +258,7 @@ class Alphabet extends FlxSpriteGroup
 		}
 
 		if(loopNum <= splitWords.length && splitWords[loopNum] != null) {
-			var spaceChar:Bool = (splitWords[loopNum] == " " || splitWords[loopNum] == "_");
+			var spaceChar:Bool = (splitWords[loopNum] == " " || (isBold && splitWords[loopNum] == "_"));
 			if (spaceChar)
 			{
 				consecutiveSpaces++;
@@ -311,7 +325,7 @@ class Alphabet extends FlxSpriteGroup
 
 				if(tmr != null) {
 					if(dialogueSound != null) dialogueSound.stop();
-					dialogueSound = FlxG.sound.play(Paths.sound('dialogue'));
+					dialogueSound = FlxG.sound.play(soundDialog);
 				}
 
 				add(letter);
@@ -333,10 +347,10 @@ class Alphabet extends FlxSpriteGroup
 
 	override function update(elapsed:Float)
 	{
-		var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
-
 		if (isMenuItem)
 		{
+			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
+
 			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
 			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
 			if(forceX != Math.NEGATIVE_INFINITY) {
@@ -346,36 +360,6 @@ class Alphabet extends FlxSpriteGroup
 			}
 		}
 
-		switch (itemType)
-		{
-			case "Classic":
-				y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * 0.48), 0.16);
-				x = FlxMath.lerp(x, (targetY * 20) + 90, 0.16);
-
-			case "Vertical":
-				y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * 0.5), 0.16);
-				x = FlxMath.lerp(x, (targetY * 0) + 308, 0.16);
-				x += targetX;
-			
-			case "C-Shape":
-				y = FlxMath.lerp(y, (scaledY * 65) + (FlxG.height * 0.39), 0.16);
-
-				x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * 70 + (FlxG.width * 0.1), 0.16);
-				if (scaledY < 0)
-					x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * 70 + (FlxG.width * 0.1), 0.16);
-
-				if (x > FlxG.width + 30)
-					x = FlxG.width + 30;
-			case "D-Shape":
-				y = FlxMath.lerp(y, (scaledY * 90) + (FlxG.height * 0.45), 0.16);
-	
-				x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * -70 + (FlxG.width * 0.35), 0.16);
-				if (scaledY < 0)
-					x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * -70 + (FlxG.width * 0.35), 0.16);
-	
-				if (x < -900)
-					x = -900;
-		}
 		super.update(elapsed);
 	}
 
@@ -462,6 +446,10 @@ class AlphaCharacter extends FlxSprite
 				x -= 20 / textSize;
 				y -= 5 * textSize;
 				offset.x = 12 * textSize;
+			case '.':
+				y += 45 * textSize;
+				x += 5 * textSize;
+				offset.x += 3 * textSize;
 		}
 	}
 
