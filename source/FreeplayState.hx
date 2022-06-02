b6ef6fea22413235a790d1f4d20cce8e83c7b29a
Gab's Swag Alt <104793558+Gabriel2019rsAlt@users.noreply.github.com>
2022-06-02 06:50:04 +0200

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -400,14 +400,11 @@ class FreeplayState extends MusicBeatState
 
 		PlayState.SONG = Song.loadFromJson(poop, songLowercase);
 		PlayState.isStoryMode = false;
-		PlayState.isFreeplay = true;
 		
 		PlayState.storyDifficulty = curDifficulty;
 
 		PlayState.storyWeek = songs[curSelected].week;
 		trace('CURRENT WEEK: ' + WeekData.getWeekFileName());
-		CharacterSelectState.characterFile = 'bf';
-		CharacterSelectState.scoreMultipliers = [1, 1, 1, 1];
 		LoadingState.loadAndSwitchState(new CharacterSelectState());
 
 		FlxG.sound.music.volume = 0;
