9ef5a9dee3c52f1fef3b37b8d07aa2169094cc1b
Gab's Swag Alt <104793558+Gabriel2019rsAlt@users.noreply.github.com>
2022-06-05 12:30:53 +0200

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -16,12 +16,11 @@ using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyStuff:Array<Dynamic> = [
-		['Easy', '-easy'],
-		['Normal', ''],
-		['Hard', '-hard']
+	public static var defaultDifficulties:Array<String> = [
+		'Easy',
+		'Normal',
+		'Hard'
 	];
-	
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
 	public static var difficulties:Array<String> = [];
